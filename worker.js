e84a9554ac5224063e4a465975450671077d4935
Turikrisztian <148880706+Turikrisztian@users.noreply.github.com>
2025-04-17 16:20:53 +0200

    worker

--- worker.js
+++ worker.js
@@ -1,11 +1,15 @@
-onmessage = function (e) {
-    if (e.data === "start") {
-      // Számolás
-      let sum = 0;
-      for (let i = 1; i <= 1000; i++) {
-        sum += i;
-      }
-      postMessage(sum);
-    }
-  };
-  
+// worker.js - Web Worker számítási fájl
+self.onmessage = function(e) {
+  const num = e.data;
+  const result = calculateFactorial(num);
+  self.postMessage(result);
+};
+ 
+function calculateFactorial(n) {
+  if (n === 0 || n === 1) return 1;
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+      result *= i;
+  }
+  return result;
+}
